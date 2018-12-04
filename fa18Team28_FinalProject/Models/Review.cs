f841f5bffc528e5281d985d96cfe45cb214e1dc5
Johnny Chan <johnnychan@utexas.edu>
2018-12-04 13:04:51 -0600

    Weird things, but i think this will fix it

--- fa18Team28_FinalProject/Models/Review.cs
+++ fa18Team28_FinalProject/Models/Review.cs
@@ -18,11 +18,8 @@ namespace fa18Team28_FinalProject.Models
         [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public int Rating { get; set; }
 
-        [Display(Name = "Author User")]
-        public string Author_user { get; set; }
-
-        [Display(Name = "Approver User")]
-        public string Approver_user { get; set; }
+        [Display(Name = "Approval Status")]
+        public bool ApprovalStatus { get; set; }
 
         //navigation properties
         public AppUser Author { get; set; }
